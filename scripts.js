57dd5582f757d86df5a20725c2ab5a0908a806e6
KannKaro <111279140+KannKaro@users.noreply.github.com>
2023-10-24 15:32:56 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,49 @@
+let tasksArray = []
+let pushTask = document.getElementById('push')
+let tasks = document.getElementById('tasks')
+let taskId = 0
+
+pushTask.addEventListener('click', () => {
+    pushTaskToArray()
+    printArray()
+    insertData()
+})
+
+function pushTaskToArray() {
+    let inputValue = document.getElementById('input').value
+    if (inputValue === '') {
+        alert('Write something')
+    }
+    else {
+        tasksArray.push({ id: taskId, text: '⦿ ' + inputValue })
+        taskId++
+    }
+}
+
+function printArray() {
+    tasks.innerHTML = '';
+
+    tasksArray.forEach(taskObj => {
+        let taskDiv = document.createElement('div');
+        taskDiv.textContent = taskObj.text;
+
+        taskDiv.addEventListener('click', () => {
+            removeTask(taskObj.id);
+        });
+
+        tasks.appendChild(taskDiv);
+    });
+}
+
+function removeTask(taskId) {
+    tasksArray = tasksArray.filter(taskObj => taskObj.id !== taskId);
+    printArray();
+}
+
+
+
+
+
+
+
+
